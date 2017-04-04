7638f9d266da02988d7a89b0d3126a50ceffe697
Dustin Byers <dbyers84@gmail.com>
2017-04-04 13:11:21 -0700

    Added baseline files for style.css, index.html, and app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* console.log('lolwut this rocks');
+
+let sushi = prompt('do you think I like sushi?');
+
+if (sushi.toLowerCase() == 'yes') {
+  alert('you are correct.');
+} */
+
+var nameArray = ['Phil', 'Dustin', 'Kevin', 'Paul', 'Adam'];
+
+alert('Welcome to the Guessing Game, try not to lose :-)');
+var RandomName = Math.floor(Math.random() * 4);
+
+var name = prompt('Is my name ' + nameArray[RandomName] '?');
+var color = prompt('What is my favorite color');
+var food = prompt('What is ')
