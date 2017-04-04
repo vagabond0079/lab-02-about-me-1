09788b48eb598978fdd9b49e9438bf0d14ca9c03
Dustin Byers <dbyers84@gmail.com>
2017-04-04 13:43:46 -0700

    added function for randmom number, arrays and prompts for each question.

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ if (sushi.toLowerCase() == 'yes') {
   alert('you are correct.');
 } */
 
-var nameArray = ['Phil', 'Dustin', 'Kevin', 'Paul', 'Adam'];
+var nameArray = ['dustin', 'phil', 'kevin', 'paul', 'adam'];
+var colorArray = ['red', 'blue', 'black', 'green', 'pink'];
+var foodArray = ['sushi', 'soup', 'italian', 'chicken', 'fruit' ];
+var languageArray = ['english', 'spanish', 'italian', 'chinese', 'german'];
+var homeArray = ['west seattle', 'queen anne', 'redmond', 'northgate', 'burien'];
+
+function randomNumber() {
+   return Math.floor(Math.random() * 4);
+}
 
 alert('Welcome to the Guessing Game, try not to lose :-)');
-var RandomName = Math.floor(Math.random() * 4);
 
-var name = prompt('Is my name ' + nameArray[RandomName] '?');
-var color = prompt('What is my favorite color');
-var food = prompt('What is ')
+var name = prompt('Is my name ' + nameArray[randomNumber()] '?');
+var color = prompt('Is my favorite color' + colorArray[randomNumber()]);
+var food = prompt('Is my favorite food ' + foodArray[randomNumber()]);
+var language = prompt('Do I speak ' + languageArray[randomNumber()]);
+var home = prompt('Do I live in ' + homeArray[randomNumber()]);
